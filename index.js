f5d933c5c6c8142e7fdc671ff732d5cd1c585c9c
Dan K <KC135Q@users.noreply.github.com>
2017-10-21 09:35:50 -0400

    Removed parser and app.use

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 3000;
 
 // boilerplate for app.use
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+
 
 app.get('/', function(req, res) {
    res.send("some text");
